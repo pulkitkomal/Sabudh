85ae4c5cc30fb21544e996197352fb0eefbb045c
Pulkit <pulkit.komal6@gmail.com>
2020-01-28 11:52:18 +0530

    28/1/20

--- json_request_sql.py
+++ json_request_sql.py
@@ -1,33 +1,25 @@
 from flask import Flask, request, jsonify
+import mysql.connector
 
-app = Flask(__name__)
 
-tasks = [
-    {
-        'id' : '1',
-        'value' : 'Samiksha',
-        'desc' : 'Student'
-    },
-    {
-        'id' : '2',
-        'value' : 'Komal',
-        'desc' : 'Student'
-    }
-]
+mydb = mysql.connector.connect(
+  host="localhost",
+  user="root",
+  passwd="",
+  database='sabudh'
+)
 
-@app.route('/task', methods=['POST'])
-def apnd():
-    id = str(int(tasks[-1]['id']) + 1)
-    desc = request.json['desc']
-    value = request.json['value']
-    post_dict = {
-        'id': id,
-        'desc': desc,
-        'value': value
-    }
-    tasks.append(post_dict)
-    print(tasks)
-    return jsonify(tasks)
+mycursor = mydb.cursor()
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port= 5000, debug=True)
+mycursor.execute("INSERT INTO json_api (id, value, desc) VALUES ('1','pulkit','student')")
+
+mydb.commit()
+
+#
+# print(mydb)
+# app = Flask(__name__)
+#
+#
+#
+# if __name__ == '__main__':
+#     app.run(host='0.0.0.0', port= 5000, debug=True)
